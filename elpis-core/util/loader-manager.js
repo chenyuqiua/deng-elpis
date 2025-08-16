5cfc28ee51818307787071fba4f589af3b2846a5
dengzhengcheng <lananabc@163.com>
2025-08-17 02:04:42 +0800

    feat(elpis-core): register glob middleware support

--- elpis-core/util/loader-manager.js
+++ elpis-core/util/loader-manager.js
@@ -1,4 +1,5 @@
 const loaders = require('../loader');
+const { sep } = require('path');
 
 module.exports = {
   /**
@@ -9,16 +10,30 @@ module.exports = {
     const { config, extend, middleware, router, routerSchema, service, controller } = loaders;
     middleware(app);
     console.log('-- [start] load middleware done --', app.middlewares);
+
     routerSchema(app);
     console.log('-- [start] load routerSchema done --', app.routerSchema);
+
     controller(app);
     console.log('-- [start] load controller done --', app.controller);
+
     service(app);
     console.log('-- [start] load service done --', app.service);
+
     config(app);
     console.log('-- [start] load config done --', app.config);
+
     extend(app);
     console.log('-- [start] load extend done --', app);
+
+    // 注册全局中间件
+    try {
+      require(`${app.businessPath}${sep}middleware.js`)(app);
+      console.log('-- [start] load global middleware done --');
+    } catch (error) {
+      console.error(`[exception] there is no middleware file: ${error}`);
+    }
+
     router(app);
     console.log('-- [start] load router done --', app);
   },
